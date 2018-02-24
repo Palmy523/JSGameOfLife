599352d153602630846767b0410592a8608fabb0
David Palmiter <Palmy523@msn.com>
2018-02-24 12:41:40 -0500

    "Configured static content for server"

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 var express = require('express');
+var path = require('path');
 
 var app = express();
-app.use(express.static('/client'));
+app.set('port', 8080);
+console.log("Serving static path: " + path.join(__dirname, 'client'));
+app.use(express.static(path.join(__dirname, 'client')));
 
 /**
  * Serve the home page if running a node js server.
  */
 function serveIndex(req, res) {
-	res.redirect("./client/gol.html");
+	res.redirect("gol.html");
 }
 
-app.get('/', serveIndex).listen(8080);
+function start() {
+	console.log("Started Server on port " + app.get('port'));
+}
+// Listen for requests
+var server = app.listen(app.get('port'), start);
+
+app.get('/', serveIndex);
